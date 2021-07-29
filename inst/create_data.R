3d868d2a6ea0b61a45c5b25105f3c9938b7f8144
Phil Bouchet <pjbouchet@gmail.com>
2021-07-29 11:17:24 +0100

    Update create_data

--- inst/create_data.R
+++ inst/create_data.R
@@ -1,8 +1,15 @@
 # Generate example dataset
-library(tidyverse)
 library(magrittr)
+library(tidyverse)
+library(janitor)
+library(ds4psy)
+library(lubridate)
 
-set.seed(1587)
+t_end <- lubridate::ceiling_date(lubridate::now(), "year") 
+t_start <- t_end - lubridate::years(5)
+set.seed(246)
+
+template_brs <- readr::read_csv(file = "/Users/philippebouchet/Google Drive/Documents/git/rjmcmc/data/brsdata_3s_socal_autec.csv") %>% janitor::clean_names()
 
 # All tibble columns shown, no colouring negative numbers, etc.
 options(tibble.width = Inf) 
@@ -10,142 +17,105 @@ options(pillar.neg = FALSE)
 options(pillar.subtle = TRUE)
 options(pillar.sigfig = 4)
 
-# Import species list and raw data
+# dat <- simulate_data(n.species = 10,
+#                      n.whales = c(1, 2, 4, 32, 15, 7, 22, 12, 9, 46),
+#                      max.trials = 3,
+#                      covariates = list(exposed = c(0, 5), sonar = c(0, 8), behaviour = c(0, 2, -12), range = 0.5),
+#                      mu = runif(n = 10, min = 60, max = 215),
+#                      phi = 20,
+#                      sigma = 20,
+#                      Lc = c(60, 65),
+#                      Rc = c(180, 190),
+#                      seed = 58690)
 
-species.list <- readr::read_csv("../rjmcmc/data/species_list.csv", col_types = cols()) %>% 
-  janitor::clean_names(.) %>% 
-  dplyr::mutate(code = tolower(code))
+dat <- simulate_data(n.species = 7,
+                     n.whales = c(1, 2, 4, 32, 15, 7, 22),
+                     max.trials = 3,
+                     covariates = list(exposed = c(0, 5), sonar = c(0, 8), behaviour = c(0, 2, -12), range = 0.5),
+                     mu = runif(n = 7, min = 60, max = 215),
+                     phi = 20,
+                     sigma = 20,
+                     Lc = c(60, 65),
+                     Rc = c(180, 190),
+                     seed = 58690)
 
-rawdat <- readr::read_csv("../rjmcmc/data/brsdata_3s_socal_autec.csv", col_types = cols()) %>% 
-  janitor::clean_names(.) %>% 
-  dplyr::mutate(species = tolower(species))
+# summary(dat)
 
-# Project 
+n.obs <- length(dat$obs$y_ij)
+dt_org <- sort(ds4psy::sample_time(from = t_start, to = t_end, size = n.obs))
 
-dat <- rawdat %>% 
-  dplyr::mutate(project = "Example_project") %>% 
-  dplyr::left_join(x = ., y = species.list, by = c("species" = "code"))
+ddf <- tibble::tibble(project = rep("Example BRS", n.obs))
+ddf$species <- dat$species$id[dat$whales$id]
+ddf <- ddf %>% 
+  dplyr::mutate(species = dplyr::case_when(species == 1 ~ "Gg",
+                                           species == 2 ~ "Ha",
+                                           species == 3 ~ "Zc",
+                                           species == 4 ~ "Bw",
+                                           species == 5 ~ "Sw",
+                                           species == 6 ~ "Oo",
+                                           species == 7 ~ "Gm",
+                                           TRUE ~ "other")) %>% 
+  dplyr::mutate(indiv = dat$whales$id) %>% 
+  dplyr::group_by(indiv) %>% 
+  dplyr::mutate(tag_id = paste0(tolower(species), rpois(n =1, lambda = 10), "_", 
+                                rpois(n = 1, lambda = runif(n = 1, min = 1, max = 1000)))) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::select(-indiv) %>% 
+  dplyr::mutate(start_time = lubridate::ymd_hms(dt_org),
+                resp_type = sample(x = template_brs$resp_type, replace = TRUE, size = n.obs),
+                resp_spl = dat$obs$y_ij,
+                resp_se_lcum = resp_spl + rgamma(n = 1, shape = 4, rate = 1),
+                max_spl = dat$obs$Rc,
+                max_se_lcum = max_spl + rgamma(n = 1, shape = 4, rate = 1),
+                censored = dat$obs$censored,
+                exp_order = dat$covariates$values$exposed,
+                exp_duration = round(runif(n.obs, min = min(template_brs$exp_duration, na.rm = TRUE), max = max(template_brs$exp_duration, na.rm = TRUE)), 1),
+                exp_signal = dat$covariates$values$sonar,
+                pre_feeding = dat$covariates$values$behaviour)
 
-# Filter species and generate random tag IDs
 
-dat <- dat %>% 
-  dplyr::filter(species %in% c("ba", "oo", "bw", "gg", "zc", "ha", "md", "sw")) %>% 
-  dplyr::group_by(tag_id) %>% 
-  dplyr::mutate(tag_id = paste0(tolower(species), rpois(n =1, lambda = 10), "_", rpois(n =1, lambda = runif(n = 1, min = 1, max = 1000)))) %>% 
+mfas <- c("MFAS", "MFA", "REAL MFA", "MFAS_DS", "MFA HELO", "REAL MFA HELO", "SOCAL_d", "REAL MFAS", "MF ALARM")
+lfas <- c("LFAS", "LFAS_DS", "LFAS_LO", "LFA")
+
+ddf <- ddf %>%
+  dplyr::mutate(min_dose = dat$obs$Lc) %>% 
+  dplyr::rowwise() %>% 
+  dplyr::mutate(exp_signal = ifelse(exp_signal == "MFAS", sample(mfas, size = 1, replace = TRUE), 
+                              sample(lfas, size = 1, replace = TRUE)),
+                pre_feeding = ifelse(pre_feeding == "Feed", TRUE, FALSE),
+                resp_time = start_time + runif(n = 1, min = 1, max = 60 * exp_duration),
+                max_spl = ifelse(censored == 0, runif(n = 1, min = resp_spl, max = 215), 
+                                 ifelse(censored == -1, min_dose, max_spl)),
+                max_se_lcum = ifelse(censored == 0, runif(n = 1, min = resp_se_lcum, max = 215), max_se_lcum)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::select(-min_dose)
+
+range.row <- template_brs[, c("resp_range", "min_range", "inferred_resp_range", "inferred_min_range")] %>% 
+  dplyr::filter_all(dplyr::any_vars(!is.na(.)))
+range.ind <- sample(1:nrow(range.row), size = nrow(ddf), replace = TRUE)
+range.val <- range.row[range.ind, ]
+
+ddf <- ddf %>% dplyr::bind_cols(., range.val)
+
+ddf <- ddf %>% 
+  dplyr::rowwise() %>% 
+  dplyr::mutate(resp_score = ifelse(censored == 0, sample(4:7, 1, TRUE), sample(0:3, 1, TRUE))) %>% 
+  dplyr::mutate(resp_spl = ifelse(resp_score > 0 & censored == 1, runif(n = 1, 60, max_spl), resp_spl)) %>% 
+  dplyr::mutate(resp_se_lcum = ifelse(resp_score > 0 & censored == 1, 
+                                      runif(n = 1, resp_spl, max_se_lcum), resp_se_lcum)) %>% 
+  dplyr::mutate(resp_spl = ifelse(censored == -1, max_spl, resp_spl),
+                resp_se_lcum = ifelse(censored == -1, max_spl, resp_se_lcum)) %>% 
   dplyr::ungroup()
-
-# Summarise mean SPL by species
-
-spl.by.species <- dat %>% 
-  dplyr::group_by(species) %>% 
-  dplyr::summarise(spl_mean = mean(resp_spl, na.rm = TRUE), spl_sd = sd(resp_spl, na.rm = TRUE)) %>% 
-  dplyr::ungroup()
-
-spl.by.species[spl.by.species$species == "ba", ]$spl_mean <- runif(1, 60, 215)
-spl.by.species[spl.by.species$species == "bw", ]$spl_mean <- runif(1, 60, 215)
-spl.by.species[spl.by.species$species %in% c("ha", "zc"), ]$spl_mean <- rep(runif(1, 60, 215), 2)
-spl.by.species[spl.by.species$species == "md", ]$spl_mean <- runif(1, 60, 215)
-spl.by.species[spl.by.species$species == "oo", ]$spl_mean <- runif(1, 60, 215)
-spl.by.species[spl.by.species$species == "sw", ]$spl_mean <- runif(1, 60, 215)
-
-dat <- dplyr::left_join(x = dat, y = spl.by.species, by = "species")
-
-dat <- dat %>% dplyr::rowwise() %>% 
-  dplyr::mutate(resp_spl = ifelse(is.na(resp_spl), NA, 
-                              rtnorm(n = 1, location = spl_mean, scale = ifelse(is.na(spl_sd), 2, spl_sd), L = 60, U = 190))) %>%
-  dplyr::mutate(resp_se_lcum = ifelse(is.na(resp_se_lcum), NA, 
-                                      resp_spl + rgamma(n = 1, shape = 1, rate = 1))) %>%
-  dplyr::mutate(max_spl = ifelse(censored == 0, runif(n = 1, min = resp_spl, max = 200), max_spl)) %>% 
-  dplyr::ungroup()
-
-# Permute values
-dat$exp_signal <- sample(dat$exp_signal, size = nrow(dat), replace = FALSE)
-dat$exp_duration <- round(runif(n = nrow(dat), min = 8, max = 70), 0)
-dat$resp_type <- sample(dat$resp_type, size = nrow(dat), replace = FALSE)
-
-for(j in 1:nrow(dat)){
-  if(!is.na(dat$resp_spl[j])){
-    dat$resp_se_lcum[j] <- dat$resp_spl[j] + rgamma(1, 0.5, 4)
-  }
   
-  if(!is.na(dat$max_spl[j])){
-    dat$max_se_lcum[j] <- dat$max_spl[j] + rgamma(1, 0.5, 4)
-  }
+for(j in 1:nrow(ddf)){
+  amount <- rgamma(n = 1, shape = 0.5, scale = 2)
+  ddf$resp_range[j] <- ddf$resp_range[j] + amount
+  ddf$min_range[j] <- ddf$min_range[j] + amount
+  ddf$inferred_resp_range[j] <- ddf$inferred_resp_range[j] + amount
+  ddf$inferred_min_range[j] <- ddf$inferred_min_range[j] + amount
 }
 
-
-# dat$resp_score <- sample(1:7, size = nrow(dat), replace = TRUE)
-
-# dat$resp_score[dat$resp_type == "Any"] <- 0
-# dat$resp_score[is.na(dat$resp_score) | dat$resp_score == "not scored"] <- 7
-# dat$resp_score <- as.numeric(dat$resp_score)
-
-# Right-censoring
-
-# is.censored <- numeric(nrow(dat))
-# for(i in seq_len(nrow(dat))){
-#   df <- dat[i, ]
-#   if(df$resp_score >=4){
-#     if(is.na(df$resp_spl)){
-#       is.censored[i] <- 1
-#     } else {
-#       if(df$resp_spl <= df$max_spl){
-#         is.censored[i] <- 0
-#       } else {
-#         is.censored[i] <- 1
-#       }
-#     }
-#   } else {
-#     is.censored[i] <- 1
-#   }
-# }
-# 
-# dat$censored <- is.censored
-# dat$resp_spl[is.censored == 1] <- NA
-
-dat <- dat %>% 
-  dplyr::mutate(pre_feeding = sample(x = dat$pre_feeding, size = nrow(dat), replace = FALSE),
-                start_time = sample(x = dat$start_time, size = nrow(dat), replace = FALSE)) 
-
-# dat <- dat %>% 
-#   dplyr::mutate(pre_feeding = sample(x = dat$pre_feeding, size = nrow(dat), replace = FALSE),
-#                 start_time = sample(x = dat$start_time, size = nrow(dat), replace = FALSE),
-#                 max_spl = rnorm(n = nrow(dat), mean = mean(dat$max_spl), sd = sd(dat$max_spl))) %>% 
-#   dplyr::rowwise() %>% 
-#   dplyr::mutate(max_spl = ifelse(max_spl < resp_spl & !is.na(resp_spl), resp_spl, max_spl)) %>% 
-#   dplyr::ungroup()
-
-# dat <- dat %>% 
-#   dplyr::rowwise() %>% 
-#   dplyr::mutate(resp_se_lcum = resp_spl + abs(rnorm(n = 1, mean = 0, sd = 10)),
-#                 max_se_lcum = max_spl + abs(rnorm(n = 1, mean = 0, sd = 10))) %>% 
-#   dplyr::ungroup()
-
-
-# Range
-
-for(i in 1:nrow(dat)){
-  if(!is.na(dat$resp_range[i])) dat$resp_range[i] <- rgamma(n = 1, shape = 1, rate = 0.1)
-  if(!is.na(dat$min_range[i])) dat$min_range[i] <- dat$resp_range[i] - rgamma(n = 1, shape = 0.01, rate = 2)
-  
-  if(dat$censored[i] == 0){ # Response range
-    if(is.na(dat$resp_range[i])){
-      dat$inferred_resp_range[i] <- rgamma(n = 1, shape = 1, rate = 0.1)
-    } else {
-      dat$inferred_resp_range[i] <- NA
-    }
-  } else {
-    if(is.na(dat$min_range[i])){
-      dat$inferred_min_range[i] <- rgamma(n = 1, shape = 1, rate = 0.1)
-    } else {
-      dat$inferred_min_range[i] <- NA
-    }
-  }
-  
-}
-
-example_brs <- dat %>% 
-  dplyr::select(-scientific_name, -common_name, -new_code, -spl_mean, -spl_sd) %>% 
+example_brs <- ddf %>% 
   dplyr::select(project, species, tag_id, start_time, resp_time, resp_type, resp_score, resp_spl, resp_se_lcum, max_spl, max_se_lcum, censored, exp_order, exp_duration, exp_signal, pre_feeding, resp_range, min_range, inferred_resp_range, inferred_min_range)
-# names(example_brs) <- names(rawdat)
+
 usethis::use_data(example_brs, overwrite = TRUE)
