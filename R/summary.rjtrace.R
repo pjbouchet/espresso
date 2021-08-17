383a69dc459bd7cdb6251a4f3b126e9c6f2403bd
Phil Bouchet <pjbouchet@gmail.com>
2021-08-17 10:00:33 +0100

    Update summary.rjtrace
    Reduced default n.top to 10, corrected total sample size, fixed error with coda.out, and added ggplot-based tile plots for model probs

--- R/summary.rjtrace.R
+++ R/summary.rjtrace.R
@@ -49,7 +49,7 @@ summary.rjtrace <- function(rj.obj,
                             convergence = TRUE,
                             gelman.rubin = 1.1,
                             model.ranks = TRUE,
-                            n.top = NULL, 
+                            n.top = 10, 
                             covariate.prob = TRUE,
                             combine.chains = FALSE){
   
@@ -66,8 +66,9 @@ summary.rjtrace <- function(rj.obj,
   cat("Thinning interval:", rj.obj$mcmc$thin, "\n")
   cat("Burn-in:", format(rj.obj$mcmc$n.burn, big.mark = ","), "\n")
   cat("Number of chains:", rj.obj$mcmc$n.chains, "\n")
-  cat("Sample size per chain:", format(rj.obj$mcmc$n.iter / rj.obj$mcmc$thin, big.mark = ","), "\n")
-  cat("Total sample size:", format(rj.obj$mcmc$n.iter * rj.obj$mcmc$n.chains, big.mark = ","), "\n\n")
+  size.per.chain <- rj.obj$mcmc$n.iter / rj.obj$mcmc$thin
+  cat("Sample size per chain:", format(size.per.chain, big.mark = ","), "\n")
+  cat("Total sample size:", format(size.per.chain * rj.obj$mcmc$n.chains, big.mark = ","), "\n\n")
   cat("Run times:\n")
   print(rj.obj$mcmc$run_time)
   
@@ -76,11 +77,11 @@ summary.rjtrace <- function(rj.obj,
   cat("--------------------\n")
   
   cat("Priors:\n\n")
-  cat("μ: Uniform", paste0("(", paste0(rj.obj$dat$param$bounds["mu", ], collapse = "; "), ")"), "\n")
-  cat("φ: Uniform", paste0("(", paste0(rj.obj$dat$param$bounds["phi", ], collapse = "; "), ")"), "\n")
-  cat("σ: Uniform", paste0("(", paste0(rj.obj$dat$param$bounds["sigma", ], collapse = "; "), ")"), "\n")
+  cat("μ: Uniform", paste0("[", paste0(rj.obj$dat$param$bounds["mu", ], collapse = "-"), "]"), "\n")
+  cat("φ: Uniform", paste0("[", paste0(rj.obj$dat$param$bounds["phi", ], collapse = "-"), "]"), "\n")
+  cat("σ: Uniform", paste0("[", paste0(rj.obj$dat$param$bounds["sigma", ], collapse = "-"), "]"), "\n")
   for(cc in rj.obj$dat$covariates$names){
-  cat(paste0(cc, ":"), "Normal", paste0("(", paste0(rj.obj$config$prior[[cc]], collapse = "; "), ")"), "\n")
+  cat(paste0(cc, ":"), "Normal", paste0("(", paste0(rj.obj$config$prior[[cc]], collapse = ", "), ")"), "\n")
   }
   cat("\n")
   cat("p(split):", rj.obj$mcmc$move$prob[1], "\n")
@@ -124,7 +125,7 @@ summary.rjtrace <- function(rj.obj,
                                  multivariate = TRUE)},
         error = function(e){ NA })
       
-      if(is.na(coda.out)){
+      if(all(is.na(coda.out))){
         cat("Convergence: Not assessed (insufficient MCMC samples)\n")
       } else {
         if(cvg$mpsrf < gelman.rubin){ 
@@ -177,10 +178,11 @@ summary.rjtrace <- function(rj.obj,
     if(rj.obj$config$model.select){
       
       res <- prob_models(input.obj = rj.obj, 
+                         n.top = n.top,
                          mlist = rj.obj$mlist, 
                          select = rj.obj$config$model.select,
                          do.combine = combine.chains,
-                         gvs = FALSE)
+                         gvs = "gvs" %in% class(rj.obj))
       
       if(combine.chains){
         
@@ -188,17 +190,53 @@ summary.rjtrace <- function(rj.obj,
         if(!is.null(n.top)) cat("\nTop ", n.top, " models:\n")
         print(head(res$model$m_prob, ifelse(is.null(n.top), 9999, n.top)))
         cat("\n")
+      
+        ggres <- dplyr::left_join(x = res$model$m_prob, rj.obj$mlist[, c("model", "group")], by = "model")
+        gg.cols <- pals::brewer.paired(max(unlist(ggres$group))) # Brewer paired
+        m.matrix <- do.call(rbind, ggres$group)
+        
+        gg.matrix <- tidyr::expand_grid(x = seq_len(rj.obj$dat$species$n), y = seq_len(n.top)) %>% 
+          dplyr::rowwise() %>% 
+          dplyr::mutate(grouping = m.matrix[y, x]) %>% 
+          dplyr::mutate(species = rj.obj$dat$species$names[y]) %>% 
+          dplyr::ungroup() %>% 
+          dplyr::mutate(y = n.top - y + 1)
+        
+        gg_model(dat = gg.matrix, colours = gg.cols, n.top = n.top, combine = TRUE)
         
       } else {
         
         for(nc in seq_len(coda::nchain(rj.obj$trace))){
-          
           if(is.null(n.top)) cat("Chain ", nc, ":\n", sep = "") else
             cat("\nTop ", n.top, " models (chain ", nc, "):\n", sep = "")
           print(head(res[[nc]]$model$m_prob, ifelse(is.null(n.top), 9999, n.top)))
           cat("\n")
-          
         } # End for loop
+          
+          gg.res <- purrr::map(.x = res, 
+             .f = ~{rtib <- dplyr::left_join(x = .x$model$m_prob,  
+                            rj.obj$mlist[, c("model", "group")], by = "model")
+          do.call(rbind, rtib$group)})
+          gg.cols <- pals::brewer.paired(max(unlist(gg.res)))
+          
+          m.matrix <- lapply(X = seq_len(n.top), 
+                           FUN = function(x) do.call(rbind, purrr::map(.x = gg.res, .f = ~.x[x, ])))
+
+          gg.matrix <- purrr::map(.x = m.matrix,
+                                  .f = ~{
+                                    tidyr::expand_grid(x = seq_len(rj.obj$dat$species$n),
+                                                       y = seq_len(rj.obj$mcmc$n.chains)) %>% 
+                                      dplyr::rowwise() %>% 
+                                      dplyr::mutate(grouping = .x[y, x]) %>% 
+                                      dplyr::mutate(species = rj.obj$dat$species$names[y]) %>% 
+                                      dplyr::ungroup() %>% 
+                                      dplyr::mutate(y = n.top - y + 1)})
+          
+          gg.tiles <- purrr::map(.x = seq_along(gg.matrix), 
+                                 .f = ~gg_model(dat = gg.matrix[[.x]], colours = gg.cols, 
+                                                n.top = n.top, combine = FALSE, no = .x))
+          patchwork::wrap_plots(gg.tiles)
+
       }
     } else {
       
