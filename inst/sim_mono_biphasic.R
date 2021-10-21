5fcfaa88e5f54acb1961e57c332b0830e60278c4
Phil Bouchet <pjbouchet@gmail.com>
2021-10-21 10:23:14 +0100

    Update example simulation code

--- inst/sim_mono_biphasic.R
+++ inst/sim_mono_biphasic.R
@@ -1,3 +1,4 @@
+# Strong biphasic pattern
 lower = 60
 upper = 215
 dose.range <- seq(lower, upper, 0.5)
@@ -5,20 +6,46 @@ mu = 140
 phi = 25
 sigma = 20
 
-psi = 0.2
-omega = 1.5
-npts = 50
-nu1 = 130
-nu2 = 155
-alpha = mean(c(nu1, nu2))
-# tau = sqrt(phi^2+sigma^2)
-tau = 22
+psi = 0.5
+omega = 0.5
+npts = 20
+nu1 = 90
+nu2 = 178
+alpha = 140
+tau = 10
+
+# Uncertainty between biphasic / monophasic (??)
+lower = 60
+upper = 215
+dose.range <- seq(lower, upper, 0.5)
+mu = 140
+phi = 25
+sigma = 20
+
+psi = 0
+omega = 2
+npts = 20
+nu1 = 115
+nu2 = 165
+alpha = 140
+tau = 25
+
+par(mfrow = c(1, 2))
+d1 <- truncnorm::dtruncnorm(x = dose.range, mean = nu1, sd = tau, a = lower, b = alpha)
+d2 <- truncnorm::dtruncnorm(x = dose.range, mean = nu2, sd = tau, a = alpha, b = upper)
+plot(dose.range, d1, col = "lightblue", type = 'l', ylab = 'density')
+lines(dose.range, d2, col = "blue", ylab = 'density')
+# lines(dose.range, pnorm(psi) * d1 + (1-pnorm(psi)) * d2, ylab = 'density')
 
 p.response.individ <- matrix(data = 0, nrow = npts, ncol = length(dose.range))
 
-pi.individ <- pnorm(q = qnorm(p = seq(0, 1, length = (npts + 2))[-c(1, (npts + 2))],
-                              mean = psi,  sd = omega))
+pi.individ <- pnorm(q = qnorm(p = seq(0, 1, length = (npts + 2))[-c(1, (npts + 2))], mean = psi, sd = omega))
                  
+# for(j in pi.individ){
+#   lines(dose.range, j * d1 + (1-j) * d2, ylab = 'density', col = "grey")
+# }
+
+
 # Dose-response curve corresponding to lower mixture component
 p.response.individ.lower <- 
   truncnorm::ptruncnorm(q = dose.range, 
